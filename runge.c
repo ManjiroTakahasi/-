8981c60f4b9dc551c53a46e87110fdcc08ffffc1
ManjiroTakahasi <73157721+ManjiroTakahasi@users.noreply.github.com>
2022-08-04 14:30:50 +0900

    Add files via upload

--- runge.c
+++ runge.c
@@ -3,62 +3,33 @@
 #include<time.h>
 #include<stdlib.h>
 
-long int nt=180000000000   # 時間ステップ数
-double dt=0.0000005                # 時間刻み幅
-int nx0=10                   # xの全体のセル数
-int ny0=10                   # yのセル数
-int dx=0.2               # xの刻み幅
-int qx = 5
-double dy=0.2                 # yの刻み幅
-int qy = 5
-int lbound=1                  # 仮想セル数
-int nx=nx0*qx+2*lbound           # xの総セル数
-int ny=ny0*qy+2*lbound           # yの総セル数
-int in_diffusion = 1;          #拡散項を入れるか入れないか.考慮する場合は1,しない場合は0
+#define nx 53
+#define ny 53
 
-double dlta=1.5
-double du1_x = 0.3
-double du1_y = dlta*du1_x
-double du2_x = 7.5*du1_x
-double du2_y = dlta*du2_x
+long int nt=180000000000;   // 時間ステップ数
+double dt=0.0000005;                // 時間刻み幅
+int nx0 = 10;
+int ny0 = 10;
+int dx=0.2;               // xの刻み幅
+int qx = 5;
+double dy=0.2;                 // yの刻み幅
+const int qy = 5;
+const int lbound=1;                  // 仮想セル数
+//int nx = nx0*qx+2*lbound;
+//int ny = ny0*qy+2*lbound;
+int in_diffusion = 1;          //拡散項を入れるか入れないか.考慮する場合は1,しない場合は0
+int every_outnum=100000;
+
+double dlta=1.5;
+double du1_x = 0.3;
+double du1_y = 1.5*0.3;
+double du2_x = 7.5*0.3;
+double du2_y = 1.5*7.5*0.3;
 
 double u1[nx][ny];
 double u2[nx][ny];
 //先に関数を宣言
 
-//0-1の乱数を返す.
-double uniform(void);
-void setup();
-void bound(double[nx][ny]);
-void u_cal();
-void laplacian1(double[nx][ny]);
-void laplacian2(double[nx][ny]);
-double R1(double,double);
-double R2(double,double);
-void boundary_left(double[nx][ny]);
-void boundary_right(double[nx][ny]);
-void boundary_upper(double[nx][ny]);
-void boundary_lower(double[nx][ny]);
-//setup内でu1,u2を初期化する際に用いる.
-void initarray(void);
-//配列Aを配列Bにコピーする
-void copyArray(double[nx][ny] A,double[nx][ny] B);
- 
-
-int main(void){
-    srand(time(NULL));
-    time_t t = time(NULL);
-    setup();
-    
-    for(int k = 0; k < nt; k++){
-        u_cal();
-        if(k+1 % every_outnum == 0){
-            print("nt__________%011d\n",k+1);
-        }
-    }
-    print("%d\n",time(NULL) - t);
-    return 0;
-}
 
 double uniform(void){
     return rand() / ((double)RAND_MAX);
@@ -76,8 +47,8 @@ void initarray(void){
 void setup(){
     initarray();
 
-    u1 = bound(u1);
-    u2 = bound(u2);
+    boundary(u1);
+    boundary(u2);
 }
 
 double R1(double a, double b){
@@ -88,34 +59,44 @@ double R2(double a, double b){
     return 3*a - 2*b;
 }
 
-void boundary_left(double[nx][ny] u){
-    for(int j = 0; j < ny; j++) u[lbound-1][j]=u[lbound][j];
+void boundary_left(){
+    for(int j = 0; j < ny; j++){
+	    u1[lbound-1][j]=u1[lbound][j];
+	    u2[lbound-1][j]=u2[lbound][j];
+    }
 }
 
-void boundary_right(double[nx][ny] u){
-    for(int j = 0; j < ny; j++) u[nx-lbound][j]=u[nx-lbound-1][j];
+void boundary_right(){
+    for(int j = 0; j < ny; j++){
+	    u1[nx-lbound][j]=u1[nx-lbound-1][j];
+	    u2[nx-lbound][j]=u2[nx-lbound-1][j];
+    }
 }
 
-void boundary_lower(double[nx][ny] u){
-    for(int i = 0; i < nx; i++) u[i][lbound-1]=u[i][lbound];
+void boundary_lower(){
+    for(int i = 0; i < nx; i++){
+	    u1[i][lbound-1]=u1[i][lbound];
+	    u2[i][lbound-1]=u2[i][lbound];
+    }
 }
 
-void boundary_upper(double[nx][ny] u){
-    for(int i = 0; i < nx; i++) u[i][ny-lbound]=u[i][ny-lbound-1];
+void boundary_upper(){
+    for(int i = 0; i < nx; i++){
+	    u1[i][ny-lbound]=u1[i][ny-lbound-1];
+	    u2[i][ny-lbound]=u2[i][ny-lbound-1];
+    }
 }
 
-void boundary(double[nx][ny] u){
-    double tmp[nx][ny];
-    copyArray(u, tmp);
-    tmp = boundary_upper(u);
-    tmp = boundary_lower(u);
-    tmp = boundary_right(u);
-    tmp = boundary_left(u);
-    u = tmp;
+void boundary(){
+    boundary_upper();
+    boundary_lower();
+    boundary_right();
+    boundary_left();
 }
 
 void u_cal(){
     double x_numerator1,x_numerator2,y_numerator1,y_numerator2;
+    double next_u1,next_u2;
     for(int i = lbound; i < nx - lbound; i++){
         for(int j = lbound; j < ny - lbound; j++){
             x_numerator1=du1_x*((u1[i+1][j]-u1[i][j])-(u1[i][j]-u1[i-1][j]));
@@ -130,14 +111,24 @@ void u_cal(){
             u2[i][j] = next_u2;
         }
     }
-    bound(u1);
-    bound(u2);
+    boundary(u1);
+    boundary(u2);
 }
 
-void copyArray(double[nx][ny] A, double[nx][ny] B){
-    for(int i = 0; i < nx; i++){
-        for(int j = 0; j < ny; j++){
-            B[i][j] = A[i][j];
+ 
+
+int main(void){
+    srand(time(NULL));
+    time_t t = time(NULL);
+    setup();
+    
+    for(int k = 0; k < nt; k++){
+	printf("k = %d\n",k);
+        u_cal();
+        if(k+1 % every_outnum  == 0){
+            printf("nt__________%011d\n",k+1);
         }
     }
+    printf("%ld\n",time(NULL) - t);
+    return 0;
 }
